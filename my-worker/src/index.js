85a5105c58f6784f61cc1899f8b5967213d77210
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2026-02-25 21:09:14 -0800

    Adding logic to test

--- my-worker/src/index.js
+++ my-worker/src/index.js
@@ -12,11 +12,90 @@ const { Client } = require("@notionhq/client")
 
 export default {
 	async fetch(request, env, ctx) {
-		const notion = new Client({
-        	auth: env.NOTION_TOKEN,
-    	})
-		data_source = env.TEST_DATA_SOURCE; //use testing db
-		console.log(`This is notion: ${notion}`)
-		return new Response(`Hello there`);
+		async function findData(notion_token, data_source) {
+			console.log("still here")
+			
+    		const notion = new Client({
+        		auth: notion_token,
+    		})
+			console.log(`${data_source}`)
+    		// Get all tasks that have the Morning, Afternoon, or Evening Tag
+    		const data = await notion.dataSources.query({
+				data_source_id: data_source,
+				filter_properties: ['Tags', 'Checkbox', 'Name'],
+				filter: {
+					"and": [
+						{
+							property: "Checkbox",
+							checkbox: { equals: true},   
+						},
+						{
+							"or": [
+								{
+									property: 'Tags',
+									multi_select: { contains: "Morning" },
+								},
+								{
+									property: 'Tags',
+									multi_select: { contains: "Afternoon"},
+								},
+								{
+									property: 'Tags',
+									multi_select: { contains: "Evening"},
+								}
+							]
+						}
+					]
+				}
+			})
+			console.log("hello?")
+			console.log(data)
+			return data;
+		}
+
+		async function removeChecks(tasks) {
+			for (let i = 0; i < tasks.results.length; i++) {
+				const result = await notion.pages.update({
+					page_id: tasks.results[i].id,
+					properties: {
+						Checkbox: {
+							checkbox: false,
+						} 
+					}
+				})
+				console.log(`Unchecked ${tasks.results[i].properties.Name.title[0].plain_text}`)
+			}
+		}
+
+		async function hasMore(data) {
+			if (data.has_more) {
+				let current_data = data
+				let has_more = data.has_more
+				while (has_more) {
+					const temp = await notion.dataSources.query({
+						data_source_id: process.env.DATA_SOURCE,
+						filter_properties: ['Tags', 'Checkbox', 'Name'],
+						start_cursor: current_data.next_cursor,
+					})
+					removeChecks(temp)
+					if (temp.has_more){
+						current_data = temp
+					} else {
+						has_more = false
+					}
+				} 
+			}
+		}
+        const notion_token = env.NOTION_TOKEN;
+		console.log("Hello there");
+		const data_source = env.TEST_DATA_SOURCE;
+		const result = findData(notion_token, data_source);
+		removeChecks(result);
+		hasMore(result);
+		return new Response(`Hello there ${data_source}`);
 	},
 };
+
+
+
+
